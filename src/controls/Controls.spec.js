eb911d3af78f10c931b2cde5a72f4e2fc75a591a
laura <lauradondiego@gmail.com>
2019-08-19 18:27:30 -0400

    dashboard tests passing

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,20 @@
 // Test away!
+import React from "react";
+import Controls from "./Controls";
+
+import { render } from "@testing-library/react";
+
+describe("<Controls />", () => {
+  it("Button says 'unlock gate' or 'lock gate' ", () => {
+    const controls = render(<Controls />);
+    // const buttonText = controls.getByText("Unlock Gate");
+    // console.log(buttonText);
+    controls.debug();
+    // displays elements debug
+    expect(controls.getByText("Close Gate").textContent).toBe("Close Gate");
+    //gets the close gate text exactly
+    expect(controls.getByText("Lock Gate")).toBeTruthy();
+    // any element within that component contains lock gate
+    // to be truthy means the value exists
+  });
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,13 @@
 // Test away!
+import React from "react";
+import Display from "./Display";
+
+import { render } from "@testing-library/react";
+
+describe("<Display />", () => {
+  it("displays if gate is open/closed and if it is locked/unlocked", () => {
+    const display = render(<Display />);
+    display.debug();
+    expect(display.getByText("Unlocked")).toBeTruthy();
+  });
+});
