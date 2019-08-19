49be529d9c457f60886f2e08f39a0b2d2fa3fac9
laura <lauradondiego@gmail.com>
2019-08-19 19:50:20 -0400

    got some display tests to work

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -8,6 +8,12 @@ describe("<Display />", () => {
   it("displays if gate is open/closed and if it is locked/unlocked", () => {
     const display = render(<Display />);
     display.debug();
-    expect(display.getByText("Unlocked")).toBeTruthy();
+    expect(display.getByText(/unlocked/i)).toBeTruthy();
+    expect(display.getByText("Open")).toBeTruthy();
+    //getbytext gives you the element or error
+    expect(display.queryByText("Closed")).toBeFalsy();
+    expect(display.queryByText("Locked")).toBeFalsy();
+    //getbyquery gives you element or null - way to test if falsy
+    // expect(display.classList.contains("red-led"));
   });
 });
